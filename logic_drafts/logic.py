a81763d51d5b0616bb18cccedd0790c90db42448
wowskii <108391629+wowskii@users.noreply.github.com>
2025-09-04 23:50:27 +0200

    added BPM getter

--- logic_drafts/logic.py
+++ logic_drafts/logic.py
@@ -1,5 +1,25 @@
-steps_per_minute = 60
+import requests
+import os
+from dotenv import load_dotenv
 
-def spm_to_bpm
+load_dotenv()
 
-# Where can i obtain bpm's of songs? scraping? api?
+BASE_URL="https://api.getsong.co/"
+API_KEY = os.getenv("BPM_API_KEY")
+
+steps_per_minute = 180
+
+def get_bpm(song,artist):
+    response = requests.get(
+        f"{BASE_URL}search/",
+        params={
+            "type": "both",
+            "lookup": f"song:{song}artist:{artist}",
+            "limit": 5,
+            "api_key": API_KEY
+        },
+    )
+    #print(response.json()['search']['title'])
+    return response.json()['search'][0]['tempo']
+
+print(get_bpm("Gold Guns Girls", "Metric"))
